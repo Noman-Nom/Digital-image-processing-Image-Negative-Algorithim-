30cb4bb26d138d37fb0dc53dbcd397793b9cb90e
Noman-Nom <noman5456318@gmail.com>
2023-11-27 11:30:05 +0500

    matrix initialize

---
+++ script.js
@@ -0,0 +1,20 @@
+const initializeMatrix = (tableId,rows,cols)=>{
+    const table = document.getElementById(tableId)
+    for (let i = 0; i <rows; i++) {
+    const row = table.insertRow(i)
+    for (let j = 0; j < cols; j++) {
+        const cell = row.insertCell(j)
+        const input = document.createElement('input')
+        input.type = 'number';
+            input.min = '0';
+            input.max = '15';
+            input.value = '0';
+            cell.appendChild(input)
+        
+        }
+        
+    }
+    // console.log(table)
+
+}
+initializeMatrix('matrix',4,4)
